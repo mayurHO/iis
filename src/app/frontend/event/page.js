a4b487735a3102ee0bd330f6fe970846cbe99bce
GauriGosavi01 <gsgosavi01@gmail.com>
2025-09-15 16:11:24 +0530

    new changes

--- src/app/frontend/event/page.js
+++ src/app/frontend/event/page.js
@@ -3,8 +3,7 @@ import React from 'react';
 const EventPage = () => {
     return (
         <div>
-            <h1>Event Page abc</h1>
-            mayur 
+            <h1>Event Page abc</h1> 
         </div>
     );
 };
