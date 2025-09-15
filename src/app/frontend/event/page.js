caf13212322c8f1def5f87b26047bebb31be9430
mayurHO <mayurkokate223@gmail.com>
2025-09-15 14:40:50 +0530

    setup done

---
+++ src/app/frontend/event/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EventPage = () => {
+    return (
+        <div>
+            <h1>Event Page</h1>
+            {/* Add your event content here */}
+        </div>
+    );
+};
+
+export default EventPage;
