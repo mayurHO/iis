90b37093922765b9bc37045175e33f4d467a7f9e
siddhi <pundesiddhi2003@gmail.com>
2025-09-15 16:20:39 +0530

    siddi

--- src/app/frontend/event/page.js
+++ src/app/frontend/event/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const EventPage = () => {
     return (
         <div>
-            <h1>Event Page abc</h1> 
+            <h1>Event Page abc tes</h1> 
         </div>
     );
 };
