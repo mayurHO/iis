b9e6af7b64e08702822ae1a9638137f8297bbe68
mayurHO <mayurkokate223@gmail.com>
2025-09-15 15:50:32 +0530

    test

--- src/app/frontend/event/page.js
+++ src/app/frontend/event/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 const EventPage = () => {
     return (
         <div>
-            <h1>Event Page</h1>
+            <h1>Event Page abc</h1>
             {/* Add your event content here */}
         </div>
     );
